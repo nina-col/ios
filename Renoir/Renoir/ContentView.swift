09d08aade3bd6ecf96ea8ecd0e924251be38e403
nina-col <147106096+nina-col@users.noreply.github.com>
2026-02-24 13:10:58 -0600

    Poryecto Renoir
    Otra vez el mismo proyecto porque creo que no lo pude sibir bien

---
+++ Renoir/Renoir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Renoir
+//
+//  Created by Magda on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Renoir/Renoir/RenoirApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenoirApp.swift
+//  Renoir
+//
+//  Created by Magda on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct RenoirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
